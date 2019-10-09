facb1c0ef562a52905a0826eeabee08dac24ae72
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-10-09 02:04:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,10 @@ def take_a_number(array, customer)
   puts "Welcome, #{customer}. You are number #{array.length} in line."
 end  
 
+def now_serving(line)
+  if line.length == 0
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}." 
+  end
+end
